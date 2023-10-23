f705912b0ca1f698fd49c8206833e3f659357639
Mutala Mutala <jonamut12.live@gmail.com>
2023-10-23 13:00:55 -0400

    Ajout de la gestion de l'ajout  d'une commande

---
+++ Commands/AddProductCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace CqrsMediatrExample.Commands
+{
+    public record AddProductCommand : IRequest<Product>
+    {
+        public Product Product { get; set; }    
+    }
+}
---
+++ Handlers/AddProductHandler.cs
@@ -0,0 +1,15 @@
+﻿using CqrsMediatrExample.Commands;
+using MediatR;
+
+namespace CqrsMediatrExample.Handlers
+{
+    public class AddProductHandler : IRequestHandler<AddProductCommand, Product>
+    {
+        private readonly FakeDataStore _fakeDataStore;
+        public AddProductHandler(FakeDataStore fakeDataStore) => _fakeDataStore = fakeDataStore;
+
+        public async Task<Product> Handle(AddProductCommand command, CancellationToken cancellationToken) => await _fakeDataStore.AddProduct(command.Product);
+
+    }
+    
+}
