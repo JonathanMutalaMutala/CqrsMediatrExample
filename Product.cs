c588de6cbc9daf623667e10df1e8ae552680076b
Mutala Mutala <jonamut12.live@gmail.com>
2023-10-23 12:04:38 -0400

    Ajout de la FakeDataStore

---
+++ Product.cs
@@ -0,0 +1,40 @@
+﻿namespace CqrsMediatrExample
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+    }
+    public class  FakeDataStore
+    {
+        private static List<Product> _products; 
+
+
+        public FakeDataStore()
+        {
+            _products = new List<Product>()
+            {
+                new Product { Id = 1,Name = "Produit 1"},
+                new Product { Id = 2,Name = "Produit 2"},
+                new Product { Id = 3,Name= "Produit 3"}
+            };
+        }
+
+        /// <summary>
+        ///  Permet d'ajouter un produit 
+        /// </summary>
+        /// <param name="product"></param>
+        /// <returns></returns>
+        public async Task AddProduct(Product product)
+        {
+            _products.Add(product);
+            await Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// Permet de recuperer tout les produits 
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IEnumerable<Product>> GetAllProducts() => await Task.FromResult(_products);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using CqrsMediatrExample;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
-
+builder.Services.AddSingleton<FakeDataStore>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
