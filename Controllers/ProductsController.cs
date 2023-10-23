f0d0f05725d3e80d16bfea58e5a75512c7ca0472
Mutala Mutala <jonamut12.live@gmail.com>
2023-10-23 11:56:21 -0400

    Ajout du controller Products

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CqrsMediatrExample.Controllers
+{
+    [Route("api/[products]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ISender _sender;
+
+        public ProductsController(ISender sender) => _sender = sender;
+
+
+    }
+}
